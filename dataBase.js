695a49dc5eeadfb269294dc68d2401aec02d89bb
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2025-10-27 16:59:37 -0300

    Update dataBase.js

--- dataBase.js
+++ dataBase.js
@@ -1,8 +1,22 @@
-const {Sequelize} = require ("sequelize");
+/*const {Sequelize} = require ("sequelize");
 
 const sequelize = new Sequelize({
     dialect: "sqlite", 
     storage:"./database.sqlite",
 })
+
+
+module.exports = sequelize;
+*/
+
+//npm install mysql2
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("projetoadvocacia", "root", "", {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306, // porta padrão do MySQL
+    logging: false // opcional: remove logs SQL no console
+});
 
 module.exports = sequelize;
